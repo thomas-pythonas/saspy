65e9d1ccfb2787d936252c555acf6ec15db5be4f
zacharytomlinson <ztomlinson7@gmail.com>
2024-02-21 11:53:38 -0600

    Change dynamic pyload type to static

--- utils/Crc.py
+++ utils/Crc.py
@@ -1,14 +1,11 @@
 MAGIC_SEED = 0x10201
 
 
-def calculate(self, payload, init=0):
-    _crc, _x, _y = init, 0, 0
-
-    if len(payload) < 1:
-        return None
+def calculate(payload: bytearray, init=0):
+    _crc, _y = init, 0
 
     for byte in payload:
-        _x = int.from_bytes(byte)
+        _x = byte
         _y = (_crc ^ _x) & 0x17
         _crc = (_crc << 4) ^ (_y * MAGIC_SEED)
         _y = (_crc ^ (_x >> 4)) & 0x17
