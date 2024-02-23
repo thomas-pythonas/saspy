7135703c46af00a56f033b83f89e446f24f63c4a
zacharytomlinson <ztomlinson7@gmail.com>
2024-02-23 17:01:40 -0600

    Comparing machine crc with my calculated crc for error checking

--- sas.py
+++ sas.py
@@ -211,7 +211,7 @@ class Sas:
         if rsp == "":
             raise NoSasConnection
 
-        mac_crc = [int(rsp[-2:]), int(rsp[-4:-2])]
+        mac_crc = [rsp[-2:], rsp[-4:-2]]
         my_crc = Crc.calculate(rsp[0:-2])
 
         print(mac_crc, my_crc)
